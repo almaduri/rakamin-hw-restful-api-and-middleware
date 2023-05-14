8881c070bbcc00a645a1884037c067fbd4a123a6
almaduri <ali.mdrw@gmail.com>
2023-05-14 23:58:32 +0700

    add delete route

--- routes/movies.js
+++ routes/movies.js
@@ -43,8 +43,15 @@ router.put('/:id', authenticateToken, async (req, res) => {
   }
 })
 
-router.delete('/:id', async (req, res) => {
-
+router.delete('/:id', authenticateToken, async (req, res) => {
+  try {
+    const { id } = req.params
+    const movie = await pool.query('delete from movies where id = $1', [id])
+    console.log(movie)
+    res.status(200).json({ message: 'Deleted Successfully' })
+  } catch (err) {
+    console.error(err)
+  }
 })
 
 module.exports = router
