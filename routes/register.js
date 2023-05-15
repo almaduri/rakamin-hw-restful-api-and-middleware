becff6821a82aecf60bd9330f6f64bed5b411544
almaduri <ali.mdrw@gmail.com>
2023-05-15 15:49:12 +0700

    add swagger

--- routes/register.js
+++ routes/register.js
@@ -1,3 +1,72 @@
+/**
+ * @swagger
+ * components:
+ *  securitySchemes:
+ *    bearerAuth:
+ *      type: http
+ *      scheme: bearer
+ *      bearerformat: JWT
+ *  schemas:
+ *    Register:
+ *      type: object
+ *      required:
+ *        - email
+ *        - gender
+ *        - password
+ *        - role
+ *      properties:
+ *        id:
+ *          type: integer
+ *          description: The auto-generated id of the user
+ *        email:
+ *          type: string
+ *          description: The email of the user
+ *        gender:
+ *          type: string
+ *          description: The gender of the user
+ *        password:
+ *          type: string
+ *          description: The password of the user
+ *        role:
+ *          type: string
+ *          description: The role of the user
+ */
+
+/**
+ * @swagger
+ * tags:
+ *  name: Register
+ *  description: Register managing API
+ * /register:
+ *  post:
+ *    summary: Add new user
+ *    tags: [Register]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              email:
+ *                type: string
+ *              gender:
+ *                type: string
+ *              password:
+ *                type: string
+ *              role:
+ *                type: string
+ *    security:
+ *      - bearerAuth: []
+ *    responses:
+ *      200:
+ *        description: OK
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Register'
+ */
+
 const express = require('express')
 const pool = require('../db')
 const router = express.Router()
